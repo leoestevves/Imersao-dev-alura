d3ce3d2630cd14f4789057b420284329fde6931f
Leonardo Esteves <leogoncalves60@gmail.com>
2024-09-09 14:04:37 -0300

    Aula05

--- app.js
+++ app.js
@@ -3,22 +3,48 @@ function pesquisar()
     // Obtém a seção HTML onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    if ((campoPesquisa == "") || (campoPesquisa == " "))
+    {        
+        return;
+    }
+
+    // Nao precisa digitar com letra maiuscula/minuscula, tudo e transformado em minusculo
+    campoPesquisa = campoPesquisa.toLowerCase();
+
     // Inicializa uma string vazia para armazenar os resultados da pesquisa
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
 
     // Itera sobre cada dado na lista de dados
-    for (let dado of dados) {
-        // Constrói o HTML para cada resultado, formatando os dados
-        resultados +=
-        `
-            <div class="item-resultado">
-                <h2>
-                    <a href="#" target="_blank">${dado.titulo}</a>
-                </h2>
-                <p class="descricao-meta">${dado.descricao}</p>
-                <a href=${dado.link} target="_blank">Mais informações</a>
-            </div>
-        `;
+    for (let dado of dados)
+    {
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags.toLowerCase();
+
+        if (titulo.includes(campoPesquisa) || tags.includes(campoPesquisa))
+        {
+            resultados +=
+            `
+                <div class="item-resultado">
+                    <h2>
+                        <a href="#" target="_blank">${dado.titulo}</a>
+                    </h2>
+                    <p class="descricao-meta">${dado.descricao}</p>
+                    <a href=${dado.link} target="_blank">Mais informações</a>
+                </div>
+            `;
+        }               
+        // Constrói o HTML para cada resultado, formatando os dados        
+    }
+
+    if (!resultados)
+    {
+        resultados = "<p>Nada foi encontrado</p>"
     }
 
     // Atribui o HTML gerado à seção de resultados
--- dados.js
+++ dados.js
@@ -3,17 +3,20 @@ let dados =
     {
         titulo : "Ayaka Kamisato",
         descricao : "Ayaka Kamisato é uma das herdeiras da prestigiada Família Kamisato de Inazuma, conhecida por sua beleza e graça. Como uma sacerdotisa do Santuário Narukami, ela carrega consigo a responsabilidade de manter a paz e a harmonia em sua nação. Sua habilidade com a espada é excepcional, e seus ataques gelados são capazes de congelar até mesmo os inimigos mais fortes.",
-        link : "https://wiki.hoyolab.com/pc/genshin/entry/32"
+        link : "https://wiki.hoyolab.com/pc/genshin/entry/32",
+        tags: "gelo espada inazuma"
     },
     {
         titulo : "Keqing",
         descricao : "Keqing é uma jovem e talentosa Yuheng, a líder da Miloguya de Liyue. Uma visionária Electro, ela possui uma mente brilhante e um senso de justiça aguçado. Keqing é conhecida por sua determinação e por desafiar as tradições, buscando sempre o progresso e a inovação. Sua habilidade com a espada é ágil e precisa, e seus ataques eletrizantes são capazes de derrotar qualquer oponente.",
-        link : "https://wiki.hoyolab.com/pc/genshin/entry/10"
+        link : "https://wiki.hoyolab.com/pc/genshin/entry/10",
+        tags: "raio espada liyue"
     },
     {
         titulo : "Raiden Shogun",
         descricao : "Raiden Shogun é a arconte de Inazuma, uma deusa poderosa e atemporal. Sob sua forma mortal, ela é conhecida por sua beleza imponente e por seu olhar penetrante. Como governante de Inazuma, Raiden Shogun busca a eternidade e a ordem, implementando leis rigorosas para proteger sua nação. Seus poderes eletromagnéticos são imensos, e ela é capaz de manipular o tempo e o espaço.",
-        link : "https://wiki.hoyolab.com/pc/genshin/entry/49?lang=en-us"
+        link : "https://wiki.hoyolab.com/pc/genshin/entry/49?lang=en-us",
+        tags: "raio espada lança inazuma arconte arcontes"
     }
 ];
 
